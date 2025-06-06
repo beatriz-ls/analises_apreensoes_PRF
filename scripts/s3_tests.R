c508f2492d4192f89dbaed0ad0c6fba1ac824a61
beatriz-ls <beatrizlimasilveira@gmail.com>
2025-06-06 13:24:30 -0300

    Reorganização relatório

--- scripts/s3_tests.R
+++ scripts/s3_tests.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(trend)
 library(dplyr)
+library(sandwich)
+library(lmtest)
 
 # import data ------------------------------------------------------------------
 
@@ -11,8 +13,9 @@ data <- data[uf == "ES"]
 
 data[, (c("uf", "estado")) := NULL]
 
-municipios_estado_presente <- c("ARACRUZ", "CACHOEIRO DE ITAPEMIRIM", "CARIACICA", "COLATINA",
-                                "LINHARES", "SAO MATEUS", "SERRA", "VILA VELHA", "VITORIA")
+municipios_estado_presente <- c("ARACRUZ", "CACHOEIRO DE ITAPEMIRIM", "CARIACICA",
+                                "COLATINA", "LINHARES", "SAO MATEUS", "SERRA",
+                                "VILA VELHA", "VITORIA")
 
 data_corte <- as.Date("2017-12-28")
 
@@ -22,7 +25,15 @@ data[, ano := year(data_apreensao)]
 
 data[, ano_mes := format(data_apreensao, "%Y-%m")]
 
-# Criar variável dummy para o Estado Presente
+data[, tempo := as.numeric(factor(ano_mes))]  # Converte ano_mes em 1, 2, 3...
+
+data[, pos_intervencao := fifelse(
+  programa == 1, 
+  tempo - min(tempo[programa == 1]),
+  0
+)]
+
+## Criar variável dummy para o Estado Presente
 data[,estado_presente := factor(ifelse(municipio_ocorrencia %in% municipios_estado_presente,
                                        "Estado Presente", "Demais municípios"))]
 
@@ -48,4 +59,32 @@ if (dados_agg[programa == 1, .N] > 0) {
   cat("\nNenhum dado disponível no período 'Depois'.\n")
 }
 
-# 
+# Modelo Interruptes Time Serie ------------------------------------------------
+
+## Pré processamneto para essa etapa
+dados_agregados <- data[, .(
+  total_apreensoes = .N,
+  programa = first(programa),
+  tempo = first(tempo),
+  pos_intervencao = first(pos_intervencao)
+), by = .(ano_mes, estado_presente)]
+
+## Modelo
+modelo_its <- lm(
+  total_apreensoes ~ tempo + estado_presente + programa + 
+    tempo:estado_presente + programa:estado_presente + 
+    pos_intervencao:estado_presente,
+  data = dados_agregados
+)
+
+## Verificando pressupostos
+
+### Tendencia parelelas
+
+dados_pre <- dados_agregados[programa == 0]
+modelo_pre <- lm(total_apreensoes ~ tempo * estado_presente, data = dados_pre)
+summary(modelo_pre)
+
+### Autocorrelação
+
+dwtest(modelo_its)
